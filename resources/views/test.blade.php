e174d83a1518fe23193cb7baa44f9876f7a7d345
Scheinderlou <jezekiel.javier@gmail.com>
2024-12-09 21:08:24 +0800

    Added extra comment

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    @vite('resources/css/app.css')
 
+    {{-- Need to enable npm run dev --}}
+    @vite('resources/css/app.css')
 </head>
 
 <body>
