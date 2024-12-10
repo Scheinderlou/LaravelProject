c908d8443f3dfcf4c7c29c0a288c3f1690af9b51
Scheinderlou <jezekiel.javier@gmail.com>
2024-12-10 14:44:18 +0800

    Moved them on a Pages Folder

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,14 +8,14 @@ class PagesController extends Controller
 {
     public function admin()
     {
-        return view('admin');
+        return view('pages.admin');
     }
     public function dashboard_qa()
     {
-        return view('dashboard_qa');
+        return view('pages.dashboard_qa');
     }
     public function dashboard_hr()
     {
-        return view('dashboard_hr');
+        return view('pages.dashboard_hr');
     }
 }
