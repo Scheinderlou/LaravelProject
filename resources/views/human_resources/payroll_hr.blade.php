0cb17512f2f56ef6b1d344eefa2d0dae44ceb50b
Scheinderlou <jezekiel.javier@gmail.com>
2024-12-20 19:50:45 +0800

    Completed payroll_view

--- resources/views/human_resources/payroll_hr.blade.php
+++ resources/views/human_resources/payroll_hr.blade.php
@@ -8,7 +8,7 @@
     <div class="container mx-auto p-6">
         <div class="mx-auto p-2 flex flex-col mb-2">
             <div class="flex justify-between items-center">
-                <h1 class="text-4xl font-bold">Payroll System</h1>
+                <h1 class="text-3xl font-bold text-gray-700">Payroll System</h1>
                 <a href="{{ route('payroll_view') }}">
                     <button class="bg-[#001F3F] text-white px-6 py-3 rounded hover:bg-blue-600 transition">
                         Back
@@ -37,19 +37,19 @@
             <div class="p-6 bg-[#008080] rounded-lg">
                 <h2 class="font-semibold text-lg mb-4">Personal Data</h2>
                 <div class="grid grid-cols-2 gap-4 ">
-                    <input placeholder="Staff ID" class="p-2 border rounded bg-[#D1D5DB] " />
-                    <input placeholder="First Name" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Last Name" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <select class="p-2 border rounded bg-[#D1D5DB] text-gray-400">
+                    <input placeholder="Staff ID" class="p-2 shadow rounded bg-[#D1D5DB]  " />
+                    <input placeholder="First Name" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Last Name" class="p-2 rounded bg-[#D1D5DB]" />
+                    <select class="p-2 rounded bg-[#D1D5DB] text-gray-400">
                         <option>Sex</option>
                         <option>Male</option>
                         <option>Female</option>
                     </select>
-                    <input placeholder="Department" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <select class="p-2 border rounded bg-[#D1D5DB] text-gray-400">
+                    <input placeholder="Department" class="p-2 rounded bg-[#D1D5DB]" />
+                    <select class="p-2 rounded bg-[#D1D5DB] text-gray-400">
                         <option>Scale</option>
                     </select>
-                    <select class="p-2 border rounded bg-[#D1D5DB] text-gray-400">
+                    <select class="p-2 rounded bg-[#D1D5DB] text-gray-400">
                         <option>Status</option>
                         <option>Paid</option>
                         <option>Unpaid</option>
@@ -60,16 +60,16 @@
             <div class="p-6 bg-[#008080] rounded-lg">
                 <h2 class="font-semibold text-lg mb-4">Bank Detail</h2>
                 <div class="grid grid-cols-2 gap-4">
-                    <select class="p-2 border rounded bg-[#D1D5DB] text-gray-400">
+                    <select class="p-2 rounded bg-[#D1D5DB] text-gray-400">
                         <option>Month</option>
                     </select>
-                    <select class="p-2 border rounded bg-[#D1D5DB] text-gray-400">
+                    <select class="p-2 rounded bg-[#D1D5DB] text-gray-400">
                         <option>Year</option>
                     </select>
-                    <input placeholder="Bank Name" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Account Number" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Account Name" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Phone Number" class="p-2 border rounded bg-[#D1D5DB]" />
+                    <input placeholder="Bank Name" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Account Number" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Account Name" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Phone Number" class="p-2 rounded bg-[#D1D5DB]" />
                 </div>
             </div>
         </div>
@@ -78,10 +78,10 @@
             <div class="p-6 bg-[#008080] rounded-lg">
                 <h2 class="font-semibold text-lg mb-4">Monthly Entries</h2>
                 <div class="grid grid-cols-2 gap-4">
-                    <input placeholder="Basic Salary" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Housing Allowance" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Transport Allowance" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <select class="p-2 border rounded bg-[#D1D5DB] text-gray-400">
+                    <input placeholder="Basic Salary" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Housing Allowance" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Transport Allowance" class="p-2 rounded bg-[#D1D5DB]" />
+                    <select class="p-2 rounded bg-[#D1D5DB] text-gray-400">
                         <option>Over-time Stipend</option>
                     </select>
                 </div>
@@ -89,11 +89,11 @@
             <div class="p-6 bg-[#008080] rounded-lg">
                 <h2 class="font-semibold text-lg mb-4">Deduction</h2>
                 <div class="grid grid-cols-2 gap-4">
-                    <input placeholder="Salary Advance" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Contributory Pension" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Tax" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="National Housing Fund" class="p-2 border rounded bg-[#D1D5DB]" />
-                    <input placeholder="Total Deduction" class="p-2 border rounded bg-[#D1D5DB]" />
+                    <input placeholder="Salary Advance" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Contributory Pension" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Tax" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="National Housing Fund" class="p-2 rounded bg-[#D1D5DB]" />
+                    <input placeholder="Total Deduction" class="p-2 rounded bg-[#D1D5DB]" />
                 </div>
             </div>
         </div>
--- resources/views/human_resources/payroll_view.blade.php
+++ resources/views/human_resources/payroll_view.blade.php
@@ -5,32 +5,31 @@
 @section('content')
 
     <div class="container mx-auto p-6">
-        <h2 class="text-2xl font-bold text-gray-700">HR Payroll</h2>
-        <div class="flex justify-end mb-6">
+        <h2 class="text-3xl font-bold text-gray-700 mb-3">HR Payroll</h2>
+        <div class="flex justify-between items-center mb-6">
+            <div class="flex gap-2">
+                <a href="{{ route('payroll_hr') }}">
+                    <button class="bg-[#001F3F] text-white px-4 py-2 text-sm rounded-md hover:bg-blue-600 transition">
+                        Create payroll
+                    </button>
+                </a>
+                <button class="bg-[#001F3F] text-white px-4 py-2 text-sm rounded-md hover:bg-blue-600 transition">
+                    Edit
+                </button>
+            </div>
+
             <div class="grid grid-cols-1 sm:grid-cols-3 gap-2">
                 <input type="text"
-                    class="text-sm py-2 px-4  border text-gray-600  border-gray-300 rounded focus:ring-2 focus:outline-none"
+                    class="text-sm py-2 px-4 border text-gray-600 border-gray-300 rounded-md focus:ring-2 focus:outline-none"
                     placeholder="Search bar">
                 <input type="date"
-                    class="text-sm py-2 px-4 border border-gray-300 text-gray-600 rounded focus:ring-2 focus:outline-none">
+                    class="text-sm py-2 px-4 border border-gray-300 text-gray-600 rounded-md focus:ring-2 focus:outline-none">
                 <select
-                    class="text-sm py-2 px-4 border border-gray-300 text-gray-600 rounded focus:ring-2 focus:outline-none">
+                    class="text-sm py-2 px-4 border border-gray-300 text-gray-600 rounded-md focus:ring-2 focus:outline-none">
                     <option value="">Filter by Status</option>
                 </select>
-
             </div>
         </div>
-        <div class="gap-3">
-            <a href="{{ route('payroll_hr') }}">
-                <button class="bg-[#001F3F] text-white px-4 py-2 text-sm rounded-md hover:bg-blue-600 transition">
-                    Create payroll
-                </button>
-            </a>
-            <button class="bg-[#001F3F] text-white px-4 py-2 text-sm rounded-md hover:bg-blue-600 transition">
-                Edit
-            </button>
-        </div>
-
 
         <div class="p-6 ">
             <h2 class="text-2xl font-bold text-center mb-6">Performances</h2>
@@ -70,7 +69,7 @@
             </div>
 
             {{-- Charts Section  --}}
-            <div class="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 mb-9">
+            <div class="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
                 <!-- Bar Chart -->
                 <div class="bg-[#008080] p-4 rounded shadow">
                     <h3 class="text-lg font-bold text-center mb-4">Average Gross Salary by Each Department</h3>
