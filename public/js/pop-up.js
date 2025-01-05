3efe671eb4d8972ef5de077466bfb207d14fd0be
Scheinderlou <jezekiel.javier@gmail.com>
2025-01-05 17:39:14 +0800

    Added employee/Edit Employee

--- public/js/pop-up.js
+++ public/js/pop-up.js
@@ -1,13 +1,7 @@
-   const modal = document.getElementById('crud-modal');
-
-   const modalToggleButton = document.querySelector('[data-modal-toggle="crud-modal"]');
-
-   const closeButton = modal.querySelector('[data-modal-toggle="crud-modal"]');
-
-   modalToggleButton.addEventListener('click', () => {
-       modal.classList.toggle('hidden');
-   });
-
-   closeButton.addEventListener('click', () => {
-       modal.classList.add('hidden');
-   });
+document.querySelectorAll('[data-modal-toggle]').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const modalId = btn.getAttribute('data-modal-toggle');
+        const modal = document.getElementById(modalId);
+        modal.classList.toggle('hidden');
+    });
+});
