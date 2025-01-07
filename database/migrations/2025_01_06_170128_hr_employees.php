a56429f8ec9cf3ffb3f50a402202d1d0dc2cdaa0
Scheinderlou <jezekiel.javier@gmail.com>
2025-01-07 13:45:09 +0800

    tiwasa ni buang

--- database/migrations/2025_01_06_170128_hr_employees.php
+++ database/migrations/2025_01_06_170128_hr_employees.php
@@ -9,22 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('hr_employees', function (Blueprint $table) {
-            $table->id('employee_id');
-            $table->string('name');
-            $table->date('date_of_birth');
-            $table->string('email');
-            $table->string('address');
-            $table->unsignedBigInteger('job_id');
-            $table->unsignedBigInteger('department_id');
+    // public function up(): void
+    // {
+    //     Schema::create('hr_employees', function (Blueprint $table) {
+    //         $table->id('employee_id');
+    //         $table->string('name');
+    //         $table->date('date_of_birth');
+    //         $table->string('email');
+    //         $table->string('address');
+    //         $table->unsignedBigInteger('job_id');
+    //         $table->unsignedBigInteger('department_id');
 
-            $table->foreign('job_id')->references('job_id')->on('hr_jobs')->onDelete('cascade');
-            $table->foreign('department_id')->references('department_id')->on('hr_departments')->onDelete('cascade');
+    //         $table->foreign('job_id')->references('job_id')->on('hr_jobs')->onDelete('cascade');
+    //         $table->foreign('department_id')->references('department_id')->on('hr_departments')->onDelete('cascade');
 
-        });
-    }
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
--- database/migrations/2025_01_06_170634_qa_quality_checks.php
+++ database/migrations/2025_01_06_170634_qa_quality_checks.php
@@ -9,23 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('qa_quality_checks', function (Blueprint $table) {
-            $table->id('check_id');
-            $table->integer('product_id');
-            $table->unsignedBigInteger('protocol_id');
-            $table->date('check_date');
-            $table->unsignedBigInteger('tester_id');
-            $table->string('status');
-            $table->text('comments');
+    // public function up(): void
+    // {
+    //     Schema::create('qa_quality_checks', function (Blueprint $table) {
+    //         $table->id('check_id');
+    //         $table->integer('product_id');
+    //         $table->unsignedBigInteger('protocol_id');
+    //         $table->date('check_date');
+    //         $table->unsignedBigInteger('tester_id');
+    //         $table->string('status');
+    //         $table->text('comments');
 
-            $table->foreign('protocol_id')->references('protocol_id')->on('qa_testing_protocols')->onDelete('cascade');
-            $table->foreign('tester_id')->references('employee_id')->on('hr_employees')->onDelete('cascade');
+    //         $table->foreign('protocol_id')->references('protocol_id')->on('qa_testing_protocols')->onDelete('cascade');
+    //         $table->foreign('tester_id')->references('employee_id')->on('hr_employees')->onDelete('cascade');
 
 
-        });
-    }
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
@@ -33,6 +33,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('qa_quality_checks');
-
     }
 };
