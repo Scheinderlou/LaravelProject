59e7c597646fac7a666ee4afdb1ad092277e3979
lagahid <lagahid19@gmail.com>
2025-01-09 00:12:36 +0800

    jabergay

--- app/Http/Controllers/employeeController.php
+++ app/Http/Controllers/employeeController.php
@@ -14,6 +14,7 @@ class employeeController extends Controller
         ->join('hr_jobs', 'hr_employees.job_id', '=', 'hr_jobs.job_id')
         ->join('hr_departments', 'hr_employees.department_id', '=', "hr_departments.department_id")
         ->select('hr_employees.employee_id', 'hr_employees.name', 'hr_jobs.job_title', 'hr_departments.department_name')
+        ->orderBy('employee_id')
         ->get();
 
         return view('human_resources.admin_page', compact('results'));
